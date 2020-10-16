aa6e4fec062d363452495dc6388cc039ad0c4df6
torbengb <torben@g-b.dk>
2020-10-16 15:56:57 +0200

    rudimentary login/logout for testinb

--- common/common.php
+++ common/common.php
@@ -3,33 +3,69 @@ session_start();
 if (empty($_SESSION['csrf'])) {
   if (function_exists('random_bytes')) {
     $_SESSION['csrf'] = bin2hex(random_bytes(32));
-  } else if (function_exists('mcrypt_create_iv')) {
+  } elseif (function_exists('mcrypt_create_iv')) {
     $_SESSION['csrf'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
   } else {
     $_SESSION['csrf'] = bin2hex(openssl_random_pseudo_bytes(32));
   }
 }
-
 // connect to the database:
 require "dbconfig.php";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $connection = new PDO($dsn, $username, $password, $options);
-$sql = "USE " . $dbname;
+$sql        = "USE " . $dbname;
 $connection->exec($sql);
-
 // common functions:
-function escape($html) {
-    return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
+function escape($html)
+{
+  return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
 }
 
-function showMessage($line = 0 , $file = "not specified" , $message = "") {
+function showMessage($line = 0, $file = "not specified", $message = "")
+{
   // usage: showMessage( __LINE__ , __FILE__ , "optional hint message" )
   //    or: showMessage( __LINE__ , __FILE__ )
   echo "An error occurred at line " . $line . " in file " . $file . "!" .
-      ( $message
-          ? "<br>Additional information:<br>" . $message
-          : "<br>No additional details were provided. Sorry about that."
-      );
+      ($message ? "<br>Additional information:<br>" . $message : "<br>No additional details were provided. Sorry about that.");
 }
+
+if (isset($_POST["login"])) {
+  if (!hash_equals($_SESSION['csrf'], $_POST['csrf']))
+    die();
+  $currentuserid = $_POST["user"];
+  $statement = $connection->prepare("
+        SELECT id, username
+        FROM users
+        WHERE id = :id
+        ");
+  $statement->bindValue(':id', $currentuserid);
+  $statement->execute();
+  $result = $statement->fetch(PDO::FETCH_ASSOC);
+  var_dump($result);echo "<br>";
+  //$currentuserid  =$result[0][0];
+  echo __LINE__."/".$result[0].".<br>";
+  echo __LINE__."/".$result[1].".<br>";
+  echo __LINE__."/".$result[0][2].".<br>";
+  echo __LINE__."/".$result[1][0].".<br>";
+  echo __LINE__."/".$result[1][1].".<br>";
+  echo __LINE__."/".$result[1][2].".<br>";
+  //$currentusername=$result[0][1];
+  //echo __LINE__."/".$currentuserid."/".$currentusername.".<br>";
+
+//  $_SESSION["currentusername"] = $_POST["username"];
+  //echo $_SESSION["currentuserid"]."/".$_SESSION["currentusername"];
+
+//*/
+}
+echo __LINE__."/".$_SESSION["currentuser"]."/".$_SESSION["currentuserid"]."/".$_SESSION["currentusername"].".<br>";
+
+$statement = $connection->prepare("
+        SELECT id, username FROM users
+        WHERE ( deleted = '0000-00-00 00:00:00' OR deleted IS NULL )
+        ORDER BY username
+        ");
+$statement->execute();
+$users = $statement->fetchAll();
+//var_dump($users);
 
--- common/header.php
+++ common/header.php
@@ -2,27 +2,50 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>TOOL||POOL</title>
+    <title>TOOL||POOL</title>
 
-	<link rel="stylesheet" href="/common/style.css">
+    <link rel="stylesheet" href="/common/style.css">
 </head>
 
 <body>
 <div class="header">
-	<h1 class="sitename"><a href="/index.php">TOOL||POOL</a> on <?php echo $_SERVER['HTTP_HOST']; ?></h1>
-  <div class="navbar">
-    <div class="topics">
-      || <a href="/index.php"><strong>Home</strong></a>
-      || <a href="/tools/list.php"><strong>Tools</strong></a>
-      || <a href="/users/list.php"><strong>Members</strong></a>
-      || <a href="/loans/list.php"><strong>Loans</strong></a>
-      || <a href="/taxonomy/list.php"><strong>Taxonomy</strong></a>
-      ||
+    <h1 class="sitename"><a href="/index.php">TOOL||POOL</a> on <?php echo $_SERVER['HTTP_HOST']; ?></h1>
+    <div class="navbar">
+        <div class="topics">
+            || <a href="/index.php"><strong>Home</strong></a>
+            || <a href="/tools/list.php"><strong>Tools</strong></a>
+            || <a href="/users/list.php"><strong>Members</strong></a>
+            || <a href="/loans/list.php"><strong>Loans</strong></a>
+            || <a href="/taxonomy/list.php"><strong>Taxonomy</strong></a>
+            || <?php echo "Hello " . escape($_SESSION['currentusername']); ?>
+
+
+            <form method="post" xaction="login.php">
+                <input type="hidden" name="csrf" value="<?php echo escape($_SESSION['csrf']); ?>">
+                <input type="hidden" name="id" value="<?php echo escape($user['id']); ?>">
+
+                <label class="label" for="user"><span class="labeltext">Switch to user:</span>
+                    <select class="input" name="user" id="user">
+                      <?php foreach ($users as $row) : ?>
+                          <option
+                                  name="user"
+                                  id="user"
+                                  value="<?php echo escape($row['id']); ?>"
+                              <?php echo(escape($row["id"]) == escape($_SESSION["currentuserid"]) ? "selected='selected'" : NULL) ?>
+                          ><?php echo escape($row['username']); ?></option>
+                      <?php endforeach; ?>
+                    </select>
+                </label>
+
+                <button class="button submit" type="submit" name="login" value="login">Login</button>
+            </form>
+
+
+        </div>
     </div>
-  </div>
 </div>
 <div class="mainbody">

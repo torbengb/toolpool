c9aed4e9f6469fd4cee6b995c8ad2fc8e1ebc0ac
torbengb <torben@g-b.dk>
2020-10-18 12:18:55 +0200

    added blog to footer

--- common/footer.php
+++ common/footer.php
@@ -9,6 +9,7 @@
     <div class="meta">
     <span>
       || <strong alt="* opens in new page" title="* opens in new page"><a href="https://github.com/torbengb/toolpool#README" target="_new">About</a></strong>
+      || <strong alt="* opens in new page" title="* opens in new page"><a href="https://blog.toolpool.club" target="_new">Blog</a></strong>
       || <strong alt="* opens in new page" title="* opens in new page"><a href="https://github.com/torbengb/toolpool" target="_new">Github</a></strong>
       || <strong alt="* opens in new page" title="* opens in new page"><a href="https://github.com/torbengb/toolpool/issues?q=is%3Aopen+is%3Aissue+label%3Abug" target="_new">Known bugs</a></strong>
       || <strong alt="* opens in new page" title="* opens in new page"><a href="https://github.com/torbengb/toolpool/issues/new" target="_new">Report a bug</a></strong>

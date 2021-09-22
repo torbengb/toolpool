f13997def120993096c6308dc0d9405643de4fe5
torbengb <torben@g-b.dk>
2021-09-22 21:37:43 +0200

    closed #39: owner and lender history

--- tools/view.php
+++ tools/view.php
@@ -53,29 +53,10 @@ if (isset($_GET['id'])) { // Action on LOAD:
     echo $urlencoded ?>&choe=UTF-8" title="URL to this page"/>
 </div>
 
-<div><h3>Lending history</h3>
-	<?php
-	$numlends  = 0;
-	$statement = $connection->prepare("
-            SELECT count(l.id) as count
-            FROM loans l 
-            WHERE l.tool = :tool_id
-            AND ( l.deleted = '0000-00-00 00:00:00' OR l.deleted IS NULL )
-            ");
-	$statement->bindValue(':tool_id', $tool_id);
-	$statement->execute();
-	$statement = $statement->fetchAll();
-	$numlends  = $statement[0][0][0];
-		if ( $numlends == 1 ) $times = " time" ;
-        else $times = " times";
-		?>
-    This tool has been lent <?php echo $numlends.$times; ?> so far.</div>
-
 <div><h3>Tool details</h3>
 <ul>
-    <li><a href="/users/view.php?id=<?php echo escape($tool["userid"]); ?>"><?php echo escape($tool["username"]); ?></a></li>
-    <li><?php echo(escape($tool["offered"]) ? "offered" : "not offered") ?></li>
     <li><?php echo escape($tool["toolname"]); ?></li>
+    <li><?php echo(escape($tool["offered"]) ? "offered" : "not offered") ?></li>
     <li><?php echo escape($tool["brand"]); ?></li>
     <li><?php echo escape($tool["model"]); ?></li>
     <li><?php echo escape($tool["weight"]); ?></li>
@@ -93,5 +74,156 @@ if (isset($_GET['id'])) { // Action on LOAD:
     <li><?php echo(escape($tool["pneumatic"]) ? "pneu" : NULL) ?></li>
 </ul>
 </div>
+
+<h3>Lending history</h3>
+<div>
+  <?php
+	$numlends  = 0;
+	$statement = $connection->prepare("
+            SELECT count(l.id) as count
+            FROM loans l 
+            WHERE l.tool = :tool_id
+            AND ( l.deleted = '0000-00-00 00:00:00' OR l.deleted IS NULL )
+            ");
+	$statement->bindValue(':tool_id', $tool_id);
+	$statement->execute();
+	$statement = $statement->fetchAll();
+	$numlends  = $statement[0][0][0];
+	if ( $numlends == 1 ) {
+		$times = " time";
+	}
+	else {
+		$times = " times";
+	}
+	?>
+    This tool has been lent <?php echo $numlends . $times; ?> so far.
+</div>
+
+<?php if ( $numlends > 0 && isset($_SESSION['currentusername']) && escape($tool["userid"]) == $_SESSION['currentuserid'] ) :
+	try { // load the record
+		$connection = new PDO($dsn, $username, $password, $options);
+		$sql        = "USE " . $dbname;
+		$connection->exec($sql);
+		$statement = $connection->prepare("
+        SELECT l.*, u.id as userid, u.username AS lender
+        FROM loans l
+		JOIN users u ON l.loanedto = u.id
+		WHERE ( l.deleted = '0000-00-00 00:00:00' OR l.deleted IS NULL )
+          AND l.tool = $tool_id
+		ORDER BY l.agreedstart DESC
+		");
+		$statement->execute();
+		$result = $statement->fetchAll();
+	} catch (PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}
+	?>
+    <table>
+        <thead>
+        <tr>
+            <th>Action</th>
+            <th>Active</th>
+            <th>Loaned to</th>
+            <th>Created</th>
+            <th>Agreed start</th>
+            <th>Agreed end</th>
+            <th>Actual start</th>
+            <th>Actual end</th>
+        </tr>
+        </thead>
+        <tbody>
+				<?php foreach ( $result as $row ) : ?>
+            <tr>
+                <td><?php if ( escape($row["active"]) == 1 ) : ?>
+                      <form method="post">
+                          <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
+                          <a href="/loans/edit.php?id=<?php echo escape($row["id"]); ?>">Edit</a>
+                      </form>
+					<?php endif; ?>
+                </td>
+                <td><?php echo(escape($row["active"]) ? "active" : "-"); ?></td>
+                <td><a href="/users/view.php?id=<?php echo escape($row["userid"]); ?>">
+										<?php echo escape($row["lender"]); ?></a>
+                </td>
+                <td><?php echo escape($row["created"]); ?></td>
+                <td><?php echo escape($row["agreedstart"]); ?></td>
+                <td><?php echo escape($row["agreedend"]); ?></td>
+                <td><?php echo escape($row["actualstart"]); ?></td>
+                <td><?php echo escape($row["actualend"]); ?></td>
+            </tr>
+				<?php endforeach; ?>
+        </tbody>
+    </table><br/>
+<?php endif; ?>
+
+<div>
+	<?php
+	$numlends2  = 0;
+	$loanedto2 = $_SESSION['currentuserid'];
+	$statement = $connection->prepare("
+            SELECT count(l.id) as count
+            FROM loans l 
+            WHERE l.tool = :tool_id
+            AND ( l.deleted = '0000-00-00 00:00:00' OR l.deleted IS NULL )
+            AND l.loanedto = $loanedto2
+            ");
+	$statement->bindValue(':tool_id', $tool_id);
+	$statement->execute();
+	$statement = $statement->fetchAll();
+	$numlends2 = $statement[0][0][0];
+	if ( $numlends2 == 1 ) {
+		$times = " time";
+	}
+	else {
+		$times = " times";
+	}
+	?>
+    You have loaned this tool <?php echo $numlends2 . $times; ?> so far:
+</div>
+
+<?php if ( $numlends > 0 && isset($_SESSION['currentusername']) && escape($tool["userid"]) != $_SESSION['currentuserid'] ) :
+	try { // load the record
+		$connection = new PDO($dsn, $username, $password, $options);
+		$sql        = "USE " . $dbname;
+		$connection->exec($sql);
+		$statement = $connection->prepare("
+        SELECT l.*
+        FROM loans l
+		WHERE ( l.deleted = '0000-00-00 00:00:00' OR l.deleted IS NULL )
+        AND l.tool = $tool_id
+        AND l.loanedto = $loanedto2
+		ORDER BY l.agreedstart DESC
+		");
+		$statement->execute();
+		$result = $statement->fetchAll();
+	} catch (PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}
+	?>
+    <table>
+        <thead>
+        <tr>
+            <th>Active</th>
+            <th>Created</th>
+            <th>Agreed start</th>
+            <th>Agreed end</th>
+            <th>Actual start</th>
+            <th>Actual end</th>
+        </tr>
+        </thead>
+        <tbody>
+		  <?php foreach ( $result as $row ) : ?>
+            <tr>
+                <td><?php echo(escape($row["active"]) ? "active" : "-"); ?></td>
+                <td><?php echo escape($row["created"]); ?></td>
+                <td><?php echo escape($row["agreedstart"]); ?></td>
+                <td><?php echo escape($row["agreedend"]); ?></td>
+                <td><?php echo escape($row["actualstart"]); ?></td>
+                <td><?php echo escape($row["actualend"]); ?></td>
+            </tr>
+				<?php endforeach; ?>
+        </tbody>
+    </table><br/>
+<?php endif; ?>
 
 <?php require "../common/footer.php"; ?>
